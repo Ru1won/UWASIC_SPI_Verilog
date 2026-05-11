aa52b39b4d96d31ec31b20c36a534bd3e21366ad
Ruwan Kadam <ruwankadam@gmail.com>
2026-05-11 12:38:57 -0400

    SPI_Peripheral_ver2

--- src/SPIperipheral.v
+++ src/SPIperipheral.v
@@ -1,49 +1,57 @@
 module SPIperipheral (
     input SCLK, nCS, COPI,
     input clk, rst_n,
-    output [15:0]bit_16_send,//process output in top module
+    output reg [15:0]bitsend,//process output in top module
     output reg transaction_validated
 );
 
 
 //declaring registers
-reg [15:0] COPI_sync1, COPI_sync2;
-reg nCS_sync1, nCS_sync2;
-reg SCLK_sync1, SCLK_sync2;
+reg COPI_sync1, COPI_sync2;
+reg nCS_sync1, nCS_sync2, nCS_prev3;
+reg SCLK_sync1, SCLK_sync2, SCLK_prev3;
+reg [15:0]bitstore;
 reg transaction_ready = 1'b0;
 reg transaction_processed = 1'b0;
+reg continue = 1'b1;
+reg [4:0] i;
 
-
-// Process SPI protocol in the clk domain, rst_n is low to reset
-integer i = 0;
+// Process SPI protocol in the clk domain
 always @(posedge clk or negedge rst_n) begin
     nCS_sync1 <= nCS;
     nCS_sync2 <= nCS_sync1;
+    nCS_prev3 <= nCS_sync2;
     SCLK_sync1 <= SCLK;
     SCLK_sync2 <= SCLK_sync1;
+    SCLK_prev3 <= SCLK_sync2;
+    COPI_sync1 <= COPI
+    COPI_sync
     if (!rst_n) begin
-        transaction_ready <= 1'b0;
-        COPI_sync1[15:0] <= 16'b0;
-        COPI_sync2[15:0] <= 16'b0;
-        i <= 0;
         //What if rst_n is hit and resets the COPI_sync registers to zero while data is still being pushed into them?
         //The registers would then be half-filled, which would result in an incorrect address and/or message.
-        //This is a non-issue since setting the COPI_sync registers to zero will result in a 'read' first bit, meaning we can ignore the broken message.
+        //Hence, the continue protocol ensures registers are not filled in after a reset is hit.
+        continue <= 1'b0;
+        i <= 0;
+        bitstore <= 16'b0;
+        transaction_ready <= 1'b0;
+        bitsend <= 16'b0;
+        transaction_validated <= 1'b0;
     end
-    else if ({(nCS_sync2 == 1'b0) & (SCLK_sync2 & !SCLK_sync1)}) begin
-            COPI_sync1[15-i] <= COPI;
-            COPI_sync2[15-i] <= COPI_sync1[15-i];
+    else if ((nCS_sync2 == 1'b0)&&(continue == 1'b1)) begin //ensure nCS is low
+        if ((SCLK_sync2 && !SCLK_prev3)&( i < 16)) begin
+            bitstore[15-i] <= COPI_sync2; //bitstore [15-i] will never go out of bounds since i < 16.
             i <= i + 1;
-            //bit[15] will be the read/write bit, and if bit[15] == 0, the message will be diregarded by the top module
+        end
     end
     else begin
-        if (nCS_sync2 & !nCS_sync1) begin //posedge of nCS
-            transaction_ready <=  1'b1;
+        // When nCS goes high (transaction ends), validate the complete transaction
+        if (nCS_sync2 && !nCS_prev3) begin
+            transaction_ready <= 1'b1;
+            continue <= 1'b1;
         end
         else if (transaction_processed) begin
             transaction_ready <= 1'b0;
-            COPI_sync1[15:0] <= 16'b0;
-            COPI_sync2[15:0] <= 16'b0;
+            bitstore[15:0] <= 16'b0;
             i <= 0;
         end
     end
@@ -53,59 +61,19 @@ end
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         transaction_processed <= 1'b1;
-        transaction_validated <= 1'b0;
-    end 
-    else if (transaction_ready && !transaction_processed) begin
-        bit_16_send[15:0] <= COPI_sync2[15:0];
-        transaction_validated <= 1'b1;
+    end else if (transaction_ready && !transaction_processed) begin
+        // Transaction is ready and not yet processed
+        bitsend[15:0] <= bitstore[15:0];
+        transaction_validated <= 1'b1; // Validation allows the values in bitsend to be read by top module.
         transaction_processed <= 1'b1;
-        end
-    else if (!transaction_ready && transaction_processed) begin
-        transaction_processed <= 1'b0;
+    end else begin
         transaction_validated <= 1'b0;
+        if (!transaction_ready && transaction_processed) begin
+        // Reset processed flag when ready flag is cleared
+        transaction_processed <= 1'b0;
+    end
     end
 end
 
+
 endmodule
-
-//___________________________________________________________________________
-//connect output based on address to these wires
-  //wire [7:0] en_reg_out_7_0; address 0x00
- //wire [7:0] en_reg_out_15_8; address 0x01
-  //wire [7:0] en_reg_pwm_7_0; address 0x02
-  //wire [7:0] en_reg_pwm_15_8; address 0x03
-  //wire [7:0] pwm_duty_cycle; address 0x04
-
-//This 'assignment' of SCLK, nCS, and COPI will happen in the top module when instantiating SPIperipheral.
-//wire SCLK, nCS, COPI;
-  //assign SCLK = ui_in[0];
-  //assign nCS = ui_in[2];
-  //assign COPI = ui_in[1];
-
-  
-//Order of operations:
-
-//Get synced signals for SCLK, COPI and nCS
-//COPIsync_1[i] <= COPI;
-//reg [15:0]COPIsync_1, [15:0]COPIsync_2;
-  //  COPIsync_2[i] <= COPIsync_1[i];
-    //i = i + 1;
-
-//Know at which cycle of clk the posedge of SCLK happens: SCLK_prev <= SCLK; {~SCLK_prev} & {SCLK} = 1
-
-//build in reset fuctionality: when rst_n goes to zero; i, COPIsync_1, COPIsync_2, = 0; nCS_sync1, nCS_sync2 = 1.
-
-//When to stop Writing to register of COPIsync_2? when i = 15
-//When to stop Writing to register COPIsync_1? when nCS_sync2 = 1.
-
-//When the reg COPIsync_2 is filled with 16 bits, Transaction_ready = 1
-
-
-//Process transaction under separate always loop by:
-
-//Add check for first R/W bit [15] --> if its zero, the command should be ignored
-
-//build in reset fuctionality: when rst_n goes to zero; transaction_validated = 0, transaction_processed = 1
-
-//Send data COPI_sync2 to bit_16_send output
-//_________________________________________________________________________________
--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Ruwan Kadam
  * SPDX-License-Identifier: Apache-2.0
  */
 `default_nettype none
@@ -15,16 +15,16 @@ module tt_um_UWASIC_onboarding_Ruwan_Kadam (
 );
   assign uio_oe = 8'hFF; // Set all IOs to output
 
-  wire [7:0] en_reg_out_7_0;
-  wire [7:0] en_reg_out_15_8;
-  wire [7:0] en_reg_pwm_7_0;
-  wire [7:0] en_reg_pwm_15_8;
-  wire [7:0] pwm_duty_cycle;
+  reg [7:0] en_reg_out_7_0;
+  reg [7:0] en_reg_out_15_8;
+  reg [7:0] en_reg_pwm_7_0;
+  reg [7:0] en_reg_pwm_15_8;
+  reg [7:0] pwm_duty_cycle;
 
-  wire [15:0] datahold;
-  wire transaction_complete;
+  reg [15:0] datahold;
+  reg transaction_complete;
 
-  SPIperipheral instSPI (.SCLK(ui_in[0]), .nCS(ui_in[2]), .COPI(ui_in[1]), .clk(clk), .rst_n(rst_n), .bit_16_send(datahold), .transaction_validated(transaction_complete));
+  SPIperipheral instSPI (.SCLK(ui_in[0]), .nCS(ui_in[2]), .COPI(ui_in[1]), .clk(clk), .rst_n(rst_n), .bitsend(datahold), .transaction_validated(transaction_complete));
 
 always @(posedge clk) begin
     if (transaction_complete && datahold[15]) begin //checking for only write bits in datahold[15]
